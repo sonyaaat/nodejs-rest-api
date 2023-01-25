1bfe39d220d1d6231f9f204a4421b2dc9e08e1f2
sonyaaat <sofiatkach2004@gmail.com>
2023-01-25 11:04:20 +0200

    Fixed2

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,16 +1,20 @@
 const Product=require("../../models/contacts")
 const listContacts = async (req, res) => {
-  let countQuery
+  // let countQuery
+  let filter
   const {id}=req.user
   const{limit=10,page=1,favorite=false}=req.query
   const skip=(page-1)*limit
-  let result= await Product.find({owner:id},"",{skip,limit:Number(limit)})
-  countQuery=await Product.where({owner:id}).countDocuments();
-  if (favorite)
+  if(favorite)
   {
-     countQuery = await Product.where({ owner:id,favorite}).countDocuments();
-     result= await Product.find({owner:id,favorite})
+    filter={ owner:id,favorite}
   }
+  else{
+    filter={owner:id}
+  }
+  const result= await Product.find(filter,"",{skip,limit:Number(limit)})
+  const countQuery = await Product.where(filter).countDocuments();
+  
   res.status(200).json({ data: result,count:countQuery });
 };
 module.exports = listContacts;
